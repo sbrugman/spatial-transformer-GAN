2059ade3da34896762631720d4e4a33388fa41bb
Simon Brugman <sbrugman@users.noreply.github.com>
2019-01-14 09:59:12 +0100

    Update preprocess_celebA.py
    Intendation

--- glasses/preprocess_celebA.py
+++ glasses/preprocess_celebA.py
@@ -46,7 +46,7 @@ def preprocess_celeb_a():
 
 		
 if __name__ == "__main__":
-        args = parse_arguments()
+    args = parse_arguments()
 	preprocess_celeb_a(args.celebA_path, args.output_path)
 
 
